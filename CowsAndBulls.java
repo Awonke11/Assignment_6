43bd55e72f9d44f7d847289ed68cf95d1533c44f
Awonke Mnotoza <93478189+Awonke11@users.noreply.github.com>
2022-09-15 17:20:36 +0200

    update CowsAndBulls.java

--- CowsAndBulls.java
+++ CowsAndBulls.java
@@ -35,6 +35,7 @@ public class CowsAndBulls {
         this.maxGuess = this.maxGuess - 1;
 
         return new Result(cows, bulls);
+        // Test
     }
 
     // ^ End the game, returning the secretNumber
